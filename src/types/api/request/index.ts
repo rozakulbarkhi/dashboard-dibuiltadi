8ce1a9225261f24a3e5d67d0cc00231ef75fc7d3
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2025-09-22 22:28:06 +0700

    feat(api): add customer and transaction related request/response types

--- src/types/api/request/index.ts
+++ src/types/api/request/index.ts
@@ -10,3 +10,50 @@ export interface RegisterRequest {
   address: string;
   password: string;
 }
+
+export interface AllCustomerRequest {
+  page: number;
+  perPage: number;
+  sortBy: string;
+  sortDirection: string;
+  startDate: string;
+  endDate: string;
+  search?: string;
+  provinceCode?: string;
+  cityCode?: string;
+}
+
+export interface CreateCustomerRequest {
+  name: string;
+  identityNo?: string;
+  npwp?: string;
+  email?: string;
+  phone?: string;
+  mobile_phone?: string;
+  provinceCode: string;
+  cityCode: string;
+  address: string;
+  companyType: string;
+}
+
+export interface UpdateCustomerRequest {
+  name: string;
+  identityNo?: string;
+  npwp?: string;
+  email?: string;
+  phone?: string;
+  mobile_phone?: string;
+}
+
+export interface GetAllTransactions {
+  page: number;
+  perPage: number;
+  sortBy: string;
+  sortDirection: string;
+  startDate: string;
+  endDate: string;
+  search?: string;
+  referenceNo?: string;
+  customerCode?: string;
+  salesCode?: string;
+}
--- src/types/api/response/index.ts
+++ src/types/api/response/index.ts
@@ -66,3 +66,152 @@ export interface TopCustomersResponse {
   responseMessage: string;
   items: TopCustomerItem[];
 }
+
+export interface AllCustomer {
+  code: string;
+  name: string;
+  type: string;
+  companyType: string;
+  area: string;
+  province: string;
+  city: string;
+  address: string;
+  group: {
+    code: null;
+    name: null;
+  };
+  status: null;
+  target: string;
+  achievement: string;
+  percentage: string;
+  createdAt: string;
+}
+
+export interface AllCustomerResponse extends BaseResponse {
+  items: AllCustomer[];
+}
+
+export interface ListCustomer {
+  code: string;
+  name: string;
+  companyType: string;
+  type: string;
+  areaCode: string;
+  province: {
+    code: string;
+    name: string;
+  };
+  city: {
+    code: string;
+    name: string;
+  };
+  subdistrict: null;
+  address: string;
+}
+
+export interface ListCustomerResponse extends BaseResponse {
+  items: ListCustomer[];
+}
+
+export type CreateCustomerResponse = BaseResponse;
+
+export interface DetailCustomer {
+  code: string;
+  name: string;
+  type: string;
+  companyType: string;
+  identityNo: string;
+  npwp: string;
+  email: string;
+  phone: string;
+  mobilePhone: string;
+  area: string;
+  province: {
+    code: string;
+    name: string;
+  };
+  city: {
+    code: string;
+    name: string;
+  };
+  address: string;
+  group: {
+    code: null;
+    name: null;
+  };
+  status: string;
+  target: string;
+  achievement: string;
+  percentage: string;
+  createdAt: string;
+}
+
+export type GetCustomerResponse = BaseResponse & {
+  item: DetailCustomer;
+};
+
+export type UpdateCustomerResponse = BaseResponse;
+
+export interface Province {
+  code: string;
+  name: string;
+}
+
+export interface City {
+  code: string;
+  name: string;
+}
+
+export interface ProvincesResponse extends BaseResponse {
+  items: Province[];
+}
+
+export interface CitiesResponse extends BaseResponse {
+  items: City[];
+}
+
+export interface Transaction {
+  referenceNo: string;
+  customer: {
+    code: string;
+    name: string;
+  };
+  sales: string;
+  amountDue: string;
+  amountUntaxed: string;
+  amountTotal: string;
+  dateOrder: string;
+  dateDue: string;
+  paidAt: string;
+  createdAt: string;
+}
+
+export interface AllTransactionsResponse extends BaseResponse {
+  items: Transaction[];
+}
+
+export interface TransactionItem {
+  productName: string;
+  quantity: string;
+  price: string;
+  discount: string;
+  priceSubtotal: string;
+  marginSubtotal: string;
+}
+
+export interface TransactionDetailResponse extends BaseResponse {
+  referenceNo: string;
+  customer: {
+    code: string;
+    name: string;
+  };
+  sales: string;
+  items: TransactionItem[];
+  amountDue: string;
+  amountUntaxed: string;
+  amountTotal: string;
+  dateOrder: string;
+  dateDue: string;
+  paidAt: string;
+  createdAt: string;
+}
